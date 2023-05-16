ec1d6c05d39d6904876ea7d2c5111f520ec96c27
Fabricio Blasich <fabricioblasich@gmail.com>
2023-05-16 20:20:56 -0300

    Ejercicio1_Lab2

---
+++ Ejercicio1_Lab2/Ejercicio1_Lab2.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity Ejercicio_Lab2 is
+
+	generic
+	(
+		DATA_WIDTH : natural := 8
+	);
+
+	port 
+	(
+
+	);
+
+end entity;
+
+architecture a_Ejercicio_Lab2 of Ejercicio_Lab2 is
+
+component deco
+	A, B, C, D	   : in std_logic;
+	
+
+end component;
+begin
+end Ejercicio_Lab2;
